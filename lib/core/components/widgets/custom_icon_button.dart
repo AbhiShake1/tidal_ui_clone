94d2a04f151ecab23a984761faf070af38f76034
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-02-08 19:01:45 +0545

    Create custom_icon_button.dart

---
+++ lib/core/components/widgets/custom_icon_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class CustomIconButton extends StatelessWidget {
+  const CustomIconButton({
+    Key? key,
+    required this.icon,
+    required this.label,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Icon(
+              icon,
+              color: context.accentColor,
+            ),
+            30.widthBox,
+            label.text.scale(1.3).bold.make(),
+          ],
+        ).pOnly(left: 15, top: 20)
+      ].toList(),
+    );
+  }
+}
